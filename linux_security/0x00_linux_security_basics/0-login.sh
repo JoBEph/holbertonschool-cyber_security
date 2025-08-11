ee17ee14df9839ffa14773f88445b90bf02afe01
JoBEph <Jordan.baroux31@gmail.com>
2025-08-11 11:24:52 +0200

    refactor: simplify login and active connections scripts by removing root checks and formatting

--- linux_security/0x00_linux_security_basics/0-login.sh
+++ linux_security/0x00_linux_security_basics/0-login.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run as root or with sudo." >&2
-	exit 1
-fi
-
 last -n 5
-#!/bin/bash
-sudo wtmpdb last -n 5
--- linux_security/0x00_linux_security_basics/1-active-connections.sh
+++ linux_security/0x00_linux_security_basics/1-active-connections.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-
-if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run as root or with sudo." >&2
-	exit 1
-fi
-
-printf "%-8s %-8s %-8s %-22s %-22s %-40s\n" "State" "Recv-Q" "Send-Q" "Local Address:Port" "Peer Address:Port" "Process"
-
-ss -tanp | awk 'NR>1 {
-	state=$1;
-	recvq=$2;
-	sendq=$3;
-	local=$4;
-	peer=$5;
-	proc="";
-	for (i=6; i<=NF; i++) proc=proc $i " ";
-	printf "%-8s %-8s %-8s %-22s %-22s %-40s\n", state, recvq, sendq, local, peer, proc;
-}'
+ss -tanp
