e1bd871f6d7d0202aba6e12ead49d77dec964579
JoBEph <Jordan.baroux31@gmail.com>
2025-08-13 11:07:03 +0200

    feat: enhance whois script to output relevant registrant information to CSV

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1"
+whois $1 | awk -F': ' '/Registrant|Admin|Tech/ {s=$1} s && $1 ~ /Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Phone Ext:|Fax|Fax Ext:|Email/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); print s" "$1","$2}' > $1.csv
