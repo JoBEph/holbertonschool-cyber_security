cf03f5237f8b7fcb3c8687f61f3b2be7a0466d92
JoBEph <Jordan.baroux31@gmail.com>
2025-09-17 12:42:09 +0200

    fix: update usage message and streamline base64 decoding in XOR decoder script

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+decoded=$(echo "$input" | base64 --decode)
 if [ -z "$1" ]; then
-    echo "Usage: $0 {xor}<string>"
+    echo "Usage: $0 {xor}<base64_string> or <base64_string>"
     exit 1
 fi
 
-input="${1#\{xor\}}"
-
-decoded=$(echo "$input" | base64 --decode)
-
-for ((i=0; i<${#decoded}; i++)); do
-    c=$(printf "%d" "'${decoded:$i:1}")
-    printf \\$(printf '%03o' $((c ^ 95)))
-done
+if [[ "$1" == \{xor\}* ]]; then
+    input="${1#\{xor\}}"
+    decoded=$(echo "$input" | base64 --decode 2>/dev/null)
+    for ((i=0; i<${#decoded}; i++)); do
+        c=$(printf "%d" "'${decoded:$i:1}")
+        printf \\$(printf '%03o' $((c ^ 95)))
+    done
+else
+    echo "$1" | base64 --decode 2>/dev/null
+fi
