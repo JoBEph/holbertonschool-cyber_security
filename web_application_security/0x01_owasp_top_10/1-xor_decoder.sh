436f15c0f57e07b6ac84d3d927612e5db6ae8afe
JoBEph <Jordan.baroux31@gmail.com>
2025-09-17 12:28:52 +0200

    fix: improve base64 decoding and output handling in XOR decoder script

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -5,18 +5,19 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-decrypt_password="${1#'{xor}'}"
+input="${1#\{xor\}}"
 
-decrypt=$(echo -n "$decrypt_password" | base64 --decode 2>/dev/null | tr -d '\0')
-
+decoded=$(echo "$input" | base64 --decode 2>/dev/null)
 if [ $? -ne 0 ]; then
-    echo "Erreur : Décodage Base64 échoué."
+    echo "Erreur : Base64 fail."
     exit 1
 fi
 
-python3 -c "
-decrypt = '$decrypt'
+output=$(python3 - <<EOF
+data = "$decoded"
 key = 95
-output = ''.join([chr(ord(c) ^ key) for c in decrypt])
-print(output, end='')
-"
+print(''.join([chr(ord(c) ^ key) for c in data]), end='')
+EOF
+)
+
+echo "$output"
