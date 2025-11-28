aab8e8c117f75ed148a245d2822dfc43e9a4a852
JoBEph <Jordan.baroux31@gmail.com>
2025-11-28 09:01:03 +0100

    try to fix

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo grep -oP "http://[a-zA-Z0-9.-]+(:[0-9]+)?(/[a-zA-Z0-9._%+-]*)*" logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+sudo grep -oP "http://[a-zA-Z0-9.-]+(:[0-9]+)?(/[a-zA-Z0-9._%+-]*)*" $1 | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
