836ab5c51d2db50660c1f31154caf0032e3ae195
JoBEph <Jordan.baroux31@gmail.com>
2025-11-28 13:38:30 +0100

    fix: update HTTP request extraction command in 1-endpoint.sh

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -oP "http://[a-zA-Z0-9.-]+(:[0-9]+)?(/[a-zA-Z0-9._%+-]*)*" logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+grep -Eo 'POST [^ ]|GET [^ ]+' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
