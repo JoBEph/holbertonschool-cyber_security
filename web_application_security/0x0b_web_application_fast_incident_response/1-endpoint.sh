9e67f5fd6dd9bb149cdce347de621de8078a4a17
JoBEph <Jordan.baroux31@gmail.com>
2025-11-28 13:39:15 +0100

    fixed endpoint

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -Eo 'POST [^ ]|GET [^ ]+' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+grep -Eo 'POST [^ ]|GET [^ ]+' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $3}'
