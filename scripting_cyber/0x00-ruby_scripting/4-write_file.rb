0fea41d88cd3d1d217a8641d6b51cfd6a1d47296
JoBEph <Jordan.baroux31@gmail.com>
2026-01-14 17:49:25 +0100

    Remove unnecessary require_relative and add spacing for improved readability in merge_json_files function

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
+
 require 'json'
-require_relative '4-writefile'
 
 def merge_json_files(file1_path, file2_path)
   data_1 = JSON.parse(File.read(file1_path))
@@ -8,4 +8,6 @@ def merge_json_files(file1_path, file2_path)
   merged_data = data_1 + data_2
   
   File.write(file2_path, JSON.pretty_generate(merged_data))
+  
+  puts 'Merged JSON written to file.json'
 end
