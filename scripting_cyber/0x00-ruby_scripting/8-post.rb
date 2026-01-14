d5840b865f051ce1a56cc2e4f13043b22e4caf75
JoBEph <Jordan.baroux31@gmail.com>
2026-01-14 22:27:03 +0100

    Simplify response handling in post_request function by removing status code checks and consolidating output

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -15,15 +15,7 @@ def post_request(url, body_params = {})
     http.request(request)
   end
 
-  status_code = response.code
-  status_message = response.message
-  puts "Response status: #{status_code} #{status_message}"
-  
-  if response.code.to_i >= 200 && response.code.to_i < 300
-    puts "Response body:"
-    data = JSON.parse(response.body)
-    puts JSON.pretty_generate(data)
-  else
-    puts "Failed to post data"
-  end
+  puts "Response status: #{response.code} #{response.message}"
+  puts "Response body:"
+  puts JSON.pretty_generate(JSON.parse(response.body))
 end
