8f27a40a61116146f2996ba9e530b05f05f6b74b
JoBEph <Jordan.baroux31@gmail.com>
2026-01-14 17:34:49 +0100

    Add function to count user IDs from a JSON file

---
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+def count_user_ids(path)
+  parsed = JSON.parse(File.read(path))
+  user_counts = Hash.new(0)
+  
+  parsed.each { |item| user_counts[item['userId']] += 1 if item['userId'] }
+  
+  user_counts.sort.each { |user_id, count| puts "#{user_id}: #{count}" }
+end
